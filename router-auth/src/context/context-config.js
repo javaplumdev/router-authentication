057c5c7336176adf53b76fc5796cc0489f072846
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-05-31 23:23:23 +0800

    update

--- router-auth/src/context/context-config.js
+++ router-auth/src/context/context-config.js
@@ -10,26 +10,21 @@ import {
 	GoogleAuthProvider,
 	signInWithPopup,
 } from 'firebase/auth';
-import { Firestore } from '@firebase/firestore';
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 import { db } from '../firebase/firebase-config';
 
 export const ContextVariable = createContext();
 
 export const ContextProvider = ({ children }) => {
 	const [user, setUser] = useState({});
-
+	const usersCollectionRef = collection(db, 'users');
+	console.log(usersCollectionRef);
 	const logIn = (email, password) => {
 		return signInWithEmailAndPassword(authApp, email, password);
 	};
 
 	const createAccount = async (email, password) => {
-		// fireStoreProvider.collection('users').doc(authApp.currentUser.uid).set({
-		// 	email: email,
-		// });
-
-		db.collection('users').doc(authApp.currentUser.uid).set({
-			email: email,
-		});
+		await addDoc(usersCollectionRef, { email: email, password: password });
 
 		return createUserWithEmailAndPassword(authApp, email, password);
 	};
